903b556eab00bacc2542849e40914e6fe31809e9
Rajesh_551 <130353615+rajeshvamsi2004@users.noreply.github.com>
2025-09-24 21:47:35 +0530

    Update Register.js

--- backend/Register.js
+++ backend/Register.js
@@ -22,7 +22,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER || 'r87921749@gmail.com',
-    pass: process.env.EMAIL_PASS || 'ycwe qgqu ltrr pfod'
+    pass: process.env.EMAIL_PASS || 'dhacvxvtrdwbdqsq'
   }
 });
 
