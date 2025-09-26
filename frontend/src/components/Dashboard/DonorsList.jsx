4df4a02fc6d94ac47609a83c4c6c21469dec3cab
Rajesh_551 <130353615+rajeshvamsi2004@users.noreply.github.com>
2025-09-26 22:24:23 +0530

    Update DonorsList.jsx

--- frontend/src/components/Dashboard/DonorsList.jsx
+++ frontend/src/components/Dashboard/DonorsList.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const API_BASE_URL = 'https://bloodconnect-now.onrender.com';
+const API_BASE_URL = 'https://bloodconnect-9fxm.onrender.com';
 
 const DonorsList = () => {
     const [donors, setDonors] = useState([]);
